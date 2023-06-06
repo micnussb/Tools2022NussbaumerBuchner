3a8ac28dfc1ff5637febcd4080736e3e33117f2b
DerMarcl <buchner.marcel@gmx.at>
2023-06-06 17:04:45 +0200

    Problem Kind

--- micnussb_DerMarcl_Android/dummyclass.java
+++ micnussb_DerMarcl_Android/dummyclass.java
@@ -4,7 +4,9 @@ public class dummyclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		public void thisIsANewMethod() { }
+		public void thisIsANewMethod() {
+			System.Out.println("Krasse Line");
+		}
 	}
 //haweduttn du nuttn
 }
